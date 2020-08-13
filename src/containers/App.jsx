a377e3de1db49a06aa66193ff3fae5825ac0f45b
Freddy Cabrera <fredjhonas2016@gmail.com>
2020-08-12 23:37:46 -0400

    Custom Hooks con fake Api

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,38 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Search from "../components/Search";
 import Categories from "../components/Categories";
 import Carousel from "../components/Carousel";
 import CarouselItem from "../components/CarouselItem";
 import Footer from "../components/Footer";
+import useInitialState from "../hooks/useInitialState";
 import "../assets/styles/App.scss";
 
+const API = "http://localhost:3000/initialState";
+
 const App = () => {
+  const [videos, categories] = useInitialState(API);
   return (
     <div className="App">
       <Header />
       <Search />
-      <Categories title="Mi Lista">
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
-
-      <Categories title="Tendencias">
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
-
-      <Categories title="Originales de Platzi Video">
-        <Carousel>
-          <CarouselItem />
-        </Carousel>
-      </Categories>
+      {categories.map(
+        (category) =>
+          videos[category].length > 0 && (
+            <Categories title={category}>
+              <Carousel>
+                {videos[category].map((item) => (
+                  <CarouselItem key={item.id} {...item} />
+                ))}
+              </Carousel>
+            </Categories>
+          )
+      )}
       <Footer />
     </div>
   );
